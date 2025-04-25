1ea8bf986cd8e82e8d140865c8b3318df2dbc1ee
mehdi <mehdi.khalloufi06@gmail.com>
2025-04-25 00:54:43 +0000

    upgrade laravel version

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,19 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-Route::get('/test',function(){
-    return ['message'=>'bonjour depuis laravel  test chi 9alwa !'];
-});
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/test', function () {
+    return ['message' => 'Bonjour depuis Laravel ! chi 9alwa wla ana f laravel azbi'];
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import api from "./api/axios";
 
 function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    api.get("/api/test")
+    api.get("api/test")
       .then(res => setMessage(res.data.message))
       .catch(err => console.error(err));
   }, []);
 
   return (
-    <div className="">
-      <h1 className="text-3xl font-bold underline"> mehdi qui testi Message depuis Laravel  test chi 9:</h1>
-      <p className="">{message}</p>
+    <div>
+      <h1>Message depuis Laravel :</h1>
+      <p>{message}</p>
     </div>
   );
 }
