db69cce505f3d057ac4e5a008bab277f00d0593a
walid-mataich <walidmataich1@gmail.com>
2025-04-25 11:44:45 +0100

    fixes

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,6 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema;
+
+
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191); // <-- add this
     }
 }
