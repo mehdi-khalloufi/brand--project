e145917d516b0d0d5365d1f25477958456cf4965
mehdi <mehdi.khalloufi06@gmail.com>
2025-05-10 23:43:17 +0000

    layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,18 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Layout from "./layouts/layout";
 
 function App() {
   const router = createBrowserRouter(
-    createRoutesFromElements(<Route path="/" element={<Home />} />)
+    createRoutesFromElements(
+      <>
+    <Route path="/" element={<Home />} />
+    <Route path="/layout" element={<Layout />} />
+    </>
+    )
   );
 
   return <RouterProvider router={router} />;
