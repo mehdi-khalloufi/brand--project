cc01999ebddda6a25d60fcbbcc550b829493c9f5
mehdi <mehdi.khalloufi06@gmail.com>
2025-05-10 13:16:51 +0000

    creatin de BDD

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from "react";
 import api from "./api/axios";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
-function App() {
+function Home() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
@@ -12,10 +18,32 @@ function App() {
 
   return (
     <div>
-      <h1>Message depuis Laravel :</h1>
+      <h1>Accueil</h1>
       <p>{message}</p>
     </div>
   );
+}
+
+function About() {
+  return <h1>À propos</h1>;
+}
+
+function NotFound() {
+  return <h1>Page non trouvée</h1>;
+}
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotFound />} />
+    </>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
